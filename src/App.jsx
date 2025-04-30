fcd3c91b93645a8fd71d9942af9e879e6a95cd50
GabrielMaschio <gabrielribeiromaschio@hotmail.com>
2025-04-30 17:54:09 -0300

    Start Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 export default function App() {
+
+  
   return (
-    <>
-    </>
+    <div className="w-full h-[100dvh] flex items-center justify-center">
+      <h1 className="text-purple-500 text-8xl">Teste</h1>
+    </div>
   )
 }
